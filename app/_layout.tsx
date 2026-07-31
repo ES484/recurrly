374d160a3bc4f451454f80c08f09045ba80fb1a9
ES484 <esraasaad478@gmail.com>
2026-08-01 00:52:09 +0300

    feat: custom font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,26 @@
 import "@/global.css";
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
 import { View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-
+SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "sans-regular": require("@/assets/fonts/PlusJakartaSans-Regular.ttf"),
+    "sans-light": require("@/assets/fonts/PlusJakartaSans-Light.ttf"),
+    "sans-medium": require("@/assets/fonts/PlusJakartaSans-Medium.ttf"),
+    "sans-bold": require("@/assets/fonts/PlusJakartaSans-Bold.ttf"),
+    "sans-semiBold": require("@/assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+    "sans-extraBold": require("@/assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+  if (!fontsLoaded) return null;
   return (
     <View className="bg-background flex-1">
       <SafeAreaView style={{ flex: 1 }}>
