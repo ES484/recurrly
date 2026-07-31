7293135b7b098ae48ee1f5558c2df9e458ca65ca
ES484 <esraasaad478@gmail.com>
2026-08-01 00:09:58 +0300

    feat: tab navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,32 +1,61 @@
+import { tabs } from "@/constants/data";
+import { theme } from "@/constants/theme";
+import clsx from "clsx";
 import { Tabs } from "expo-router";
+import { Image, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-export default function RootLayout() {
+export default function TabLayout() {
+  const insets = useSafeAreaInsets();
+  const { components, colors } = theme;
+  const TabBarIcon = ({ focused, icon }: TabIconProps) => {
+    return (
+      <View className="tabs-icon">
+        <View className={clsx("tabs-pill", focused && "tabs-active")}>
+          <Image source={icon} className="tabs-glyph" resizeMode="contain" />
+        </View>
+      </View>
+    );
+  };
+  const tabsItems = tabs.map((tab: AppTab) => (
+    <Tabs.Screen
+      key={tab.name}
+      name={tab.name}
+      options={{
+        title: tab.title,
+        tabBarIcon: ({ focused }) => (
+          <TabBarIcon focused={focused} icon={tab.icon} />
+        ),
+      }}
+    />
+  ));
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-        }}
-      />
-      <Tabs.Screen
-        name="subscriptions"
-        options={{
-          title: "Subscriptions",
-        }}
-      />
-      <Tabs.Screen
-        name="insights"
-        options={{
-          title: "insights",
-        }}
-      />
-      <Tabs.Screen
-        name="settings"
-        options={{
-          title: "Settings",
-        }}
-      />
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          position: "absolute",
+          bottom: 0,
+          height: components.tabBar.height,
+          marginHorizontal: components.tabBar.horizontalInset,
+          borderRadius: components.tabBar.radius,
+          backgroundColor: colors.primary,
+          borderTopWidth: 0,
+          elevation: 0,
+        },
+        tabBarItemStyle: {
+          paddingVertical:
+            components.tabBar.height / 2 - components.tabBar.iconFrame / 1.6,
+        },
+        tabBarIconStyle: {
+          width: components.tabBar.iconFrame,
+          height: components.tabBar.iconFrame,
+          alignSelf: "center",
+        },
+      }}
+    >
+      {tabsItems}
     </Tabs>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,14 @@
 import "@/global.css";
 import { Stack } from "expo-router";
+import { View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <Stack screenOptions={{ headerShown: false }} />
-    </SafeAreaView>
+    <View className="bg-background flex-1">
+      <SafeAreaView style={{ flex: 1 }}>
+        <Stack screenOptions={{ headerShown: false }} />
+      </SafeAreaView>
+    </View>
   );
 }
