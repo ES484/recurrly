1c1c829b441bf2d6287dd404abe6a10c0156732d
ES484 <esraasaad478@gmail.com>
2026-08-01 01:03:11 +0300

    fix: handle fonts error - rename font aliases

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,20 +6,20 @@ import { View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
-  const [fontsLoaded] = useFonts({
+  const [fontsLoaded, fontError] = useFonts({
     "sans-regular": require("@/assets/fonts/PlusJakartaSans-Regular.ttf"),
     "sans-light": require("@/assets/fonts/PlusJakartaSans-Light.ttf"),
     "sans-medium": require("@/assets/fonts/PlusJakartaSans-Medium.ttf"),
     "sans-bold": require("@/assets/fonts/PlusJakartaSans-Bold.ttf"),
-    "sans-semiBold": require("@/assets/fonts/PlusJakartaSans-SemiBold.ttf"),
-    "sans-extraBold": require("@/assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+    "sans-semibold": require("@/assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+    "sans-extrabold": require("@/assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
   });
 
   useEffect(() => {
-    if (fontsLoaded) {
+    if (fontsLoaded || fontError) {
       SplashScreen.hideAsync();
     }
-  }, [fontsLoaded]);
+  }, [fontsLoaded, fontError]);
   if (!fontsLoaded) return null;
   return (
     <View className="bg-background flex-1">
