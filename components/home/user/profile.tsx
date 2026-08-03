10ac9cc2a5edf51b0c57400748e984420fa7fdad
ES484 <esraasaad478@gmail.com>
2026-08-03 17:04:28 +0300

    feat: subscriptions creation

--- components/home/user/profile.tsx
+++ components/home/user/profile.tsx
@@ -3,17 +3,14 @@ import { icons } from "@/constants/icons";
 import images from "@/constants/images";
 import { Image, Pressable, Text, View } from "react-native";
 
-const UserProfile = () => {
+const UserProfile = ({ onAdd }) => {
   return (
     <View className="home-header">
       <View className="home-user">
         <Image source={images.avatar} className="home-avatar" />
         <Text className="home-user-name">{HOME_USER.name}</Text>
       </View>
-      <Pressable
-        className="home-add-icon-container"
-        onPress={() => console.log("add")}
-      >
+      <Pressable className="home-add-icon-container" onPress={onAdd}>
         <Image source={icons.add} className="home-add-icon" />
       </Pressable>
     </View>
